572065a73ab363c93966df1333a0b336f2c390f9
winstontom <93392068+winstontom@users.noreply.github.com>
2021-12-10 13:52:20 -0500

    Update plugin.js

--- plugin.js
+++ plugin.js
@@ -1,12 +1,14 @@
 // plugin.js to use with index.html file.
 // Get and display current time
+const month = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+
 function dateCalcNow() {
 	var d = new Date();
 	// document.getElementById("nowTime").innerHTML = "Time : " + d.getTime();
 	// document.getElementById("nowTime").innerHTML = "Time : " + d.getHours() + ":" + d.getMinutes() + d.getSeconds();
 	var timeStringNew = timeString(d);
 	document.getElementById("nowTime").innerHTML = "Time : " + timeStringNew;
-	document.getElementById("nowMonth").innerHTML = "Month : " + Number(d.getMonth()+1);
+	document.getElementById("nowMonth").innerHTML = "Month : " + month[d.getMonth()];
 	document.getElementById("nowDay").innerHTML = "Day : " + d.getDate();
 	document.getElementById("nowYear").innerHTML = "Year : " + d.getFullYear();
 }
@@ -14,6 +16,22 @@ function dateCalcNow() {
 //Computer Time Until an Event
 function dateCalcDiff() {
 	var d = new Date();
+	var myDate = new Date(2022, 9, 28, 12, 0, 1);
+	document.getElementById("nowDate").innerHTML = '<h3 style="border:2px solid Blue;">' + "Target Date is : " + myDate.toString() + "</h3>";
+	// There are 1,000 milliseconds in a second
+	const millisecMinute = 60000;
+	const millisecHour = 3600000;
+	const millisecDay = 86400000;
+	var timeDiff = myDate - d;
+	var daysDiff = Math.floor(timeDiff / millisecDay);
+	var hoursDiff = Math.floor((timeDiff % millisecDay) / millisecHour);
+	var minutesDiff = Math.floor((timeDiff % millisecHour) / millisecMinute);
+	var secondsDiff = Math.floor((timeDiff % millisecMinute) / 1000 );
+	
+	document.getElementById("nowTime").innerHTML = "Days Remaining : " + "&nbsp &nbsp" + daysDiff.toString();
+	document.getElementById("nowMonth").innerHTML = "Hours Remaining : " + "&nbsp &nbsp" + hoursDiff.toString();
+	document.getElementById("nowDay").innerHTML = "Minutes Remaining : " + "&nbsp &nbsp" + minutesDiff.toString();
+	document.getElementById("nowYear").innerHTML = "Seconds Remaining : " + "&nbsp &nbsp" + secondsDiff.toString();
 }
 
 // Function to create a time in the hh:mm:ss format given a date value.
